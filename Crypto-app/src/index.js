04deaf059e0e583c27b28c7358361fd630c1c96d
nassra <nassra96@gmail.com>
2020-11-24 18:44:16 +0000

    'react'

--- Crypto-app/src/index.js
+++ Crypto-app/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CryptoInfo from './CryptoInfo'
+import './App.css'
+class Main extends React.Component {
+  render() {
+    return (
+     <div className="container">
+        <h1 className="title">React Crypto Price Tracker</h1>
+      <App  />
+<CryptoInfo  />
+      </div>
+     
+    );
+  }
+}
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main  />, 
   document.getElementById('root')
 );
